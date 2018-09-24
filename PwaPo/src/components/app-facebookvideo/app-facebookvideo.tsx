02228f0d07704b383c8c04fae77255d2f831f7f0
pMasquilier <pierre.masquilier@soprasteria.com>
2018-09-24 15:44:47 +0200

    Custom de l'app-facebook-video

--- PwaPo/src/components/app-facebookvideo/app-facebookvideo.tsx
+++ PwaPo/src/components/app-facebookvideo/app-facebookvideo.tsx
@@ -1,4 +1,4 @@
-import { Component, Listen, State } from '@stencil/core';
+import { Component, State } from '@stencil/core';
 
 @Component({
   tag: 'app-facebookvideo',
@@ -6,7 +6,13 @@ import { Component, Listen, State } from '@stencil/core';
 })
 export class AppFacebookVideo {
   @State() componentHeight: number;
-  @Listen('window:resize')
+  //@Listen('window:resize')
+
+  @State() isVideoRunning: boolean;
+
+  constructor() {
+    this.isVideoRunning = false;
+  }
   windowResize() {
     this.changeComponentHeight();
   }
@@ -21,13 +27,53 @@ export class AppFacebookVideo {
     console.log(width);
     this.componentHeight = width / 1.75;
   }
+
+  lireVideo(event) {
+    console.log("event lireVideo " + event);
+    this.isVideoRunning = true;
+  }
+
+  fermerVideo(event) {
+    console.log("event fermerVideo " + event);
+    this.isVideoRunning = false;
+  }
+
   render() {
+    let video;
+
+    if (this.isVideoRunning) {
+      video = (<div class="card">
+        
+          <ion-icon name="close" onClick={(event: UIEvent) => this.fermerVideo(event)}></ion-icon>
+        
+        <ion-item>
+
+          <div class="video-container">
+            <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fsoprasteria%2Fvideos%2F1758810114153796%2F" frameborder="0" scrolling="no" width="100%" height={this.componentHeight + 'px'}></iframe >
+          </div>
+        </ion-item>
+      </div>)
+    }
 
     return (
 
-      <iframe
-        src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fsoprasteria%2Fvideos%2F1758810114153796%2F" frameborder="0" scrolling="no" width="100%" height={this.componentHeight + 'px'}></iframe >
+      <div>
+        <ion-grid>
+          <ion-row justify-content-center>
+            <ion-col size="4">
+              <ion-icon name="play" onClick={(event: UIEvent) => this.lireVideo(event)}></ion-icon>
+              <img src="../../assets/pictures/mobileFactoryVideo.PNG" onClick={(event: UIEvent) => this.lireVideo(event)}>
+              </img>
+            </ion-col>
+            <ion-col size="4">
+              <div class = "text">
+                En savoir plus sur la Mobile Factory
+              </div>
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+        {video}
+      </div>);
 
-    );
   }
 }
